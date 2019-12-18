b38c5c688b9e30b832431227bdbad8ede995f657
Hansol Tak <takyhsoul@gmail.com>
2019-12-19 00:49:22 +0900

    191218 실습했던 코드

---
+++ pro08/src/sec01/ex02/SecondServlet.java
@@ -0,0 +1,21 @@
+package sec01.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet("/second")
+public class SecondServlet extends HttpServlet{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      response.setContentType("text/html;charset=utf-8");
+      PrintWriter out = response.getWriter();
+      out.println("<html><body>");
+      out.println("SecondServlet) refresh를 이용한 Forwarding");
+      out.println("</body></html>");
+   }
+}
